21b7ca5770c65a2ed23cd8f6847d0304ecaeab73
Mohamed-Abbas-Homani <124496964+Mohamed-Abbas-Homani@users.noreply.github.com>
2023-10-21 10:04:16 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-cd frontend&&
-npm i&&
-npm run build&&
-cd ..&&
+pip install django djangorestframework django-cors-headers &&
+cd frontend &&
+npm i &&
+npm run build &&
+cd .. &&
 python manage.py runserver
